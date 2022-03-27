e0f30fa8dc0ed3745944e117631941a181a552b8
Maxwell Beck <max@rastertail.net>
2022-03-26 23:19:14 -0500

    Implement progress logging in status bar
    Needs some cleanup, but it is functional

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,75 @@ import commandExists from "command-exists";
 import cdp from "chrome-remote-interface";
 import * as vscode from "vscode";
 
+function leftTruncate(s: string, len: number): string {
+    if (s.length < len) {
+        return s;
+    } else {
+        return "..." + s.substring(s.length - len + 3);
+    }
+}
+
+// From https://github.com/NixOS/nix/blob/6a8f1b548fc85af7e065feee93920839ec94fa40/src/libutil/logging.hh
+enum ActivityType {
+    Unknown = 0,
+    CopyPath = 100,
+    FileTransfer = 101,
+    Realise = 102,
+    CopyPaths = 103,
+    Builds = 104,
+    Build = 105,
+    OptimiseStore = 106,
+    VerifyPaths = 107,
+    Substitute = 108,
+    QueryPathInfo = 109,
+    PostBuildHook = 110,
+    BuildWaiting = 111,
+}
+
+enum ResultType {
+    FileLinked = 100,
+    BuildLogLine = 101,
+    UntrustedPath = 102,
+    CorruptedPath = 103,
+    SetPhase = 104,
+    Progress = 105,
+    SetExpected = 106,
+    PostBuildLogLine = 107,
+}
+
+interface LogAction {
+    level: number,
+    msg: string,
+}
+
+interface StartAction {
+    id: number,
+    level: number,
+    type: ActivityType,
+    text: string,
+    fields: any,
+}
+
+interface StopAction {
+    id: number,
+}
+
+interface ResultAction {
+    id: number,
+    type: ResultType,
+    fields: any,
+}
+
+interface ActivityProgress {
+    done: number,
+    expected: number,
+}
+
+interface DisplayString {
+    s: string,
+    level: number,
+}
+
 async function injectEnvironment(vars: Map<string, string>) {
     // Assume parent process is the root VSCode process
     const rootProc = process.ppid;
@@ -45,13 +114,13 @@ export async function activate(ctx: vscode.ExtensionContext) {
         nixFound
     );
 
+    // TODO Make sure Nix has flake support enabled
+
     // Alert the user if Nix is not found and exit
     if (!nixFound) {
         vscode.window.showErrorMessage("Nix command not found. Flake integrations will be disabled.");
         return;
     }
-
-    // TODO Make sure Nix has flake support enabled
 
     // Create status bar item, potentially setting content
     const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
@@ -98,13 +167,141 @@ export async function activate(ctx: vscode.ExtensionContext) {
         let vars = new Map();
         const envProc = child.spawn(nixCommand, args, { cwd: flakeDir, env: { PATH: process.env["PATH"] } });
         envProc.stdout.on("data", (data: Buffer) => {
-            for (const line of data.toString().split("\n")) {
+            for (const line of data.toString().split('\n')) {
                 const eq = line.indexOf("=");
                 vars.set(line.substring(0, eq), line.substring(eq + 1));
             }
         });
 
-        // TODO Log progress
+        // Log progress
+        const progress = {
+            activities: new Map<number, ActivityProgress>(),
+            done: 0,
+            expected: 0,
+        };
+        const activityStrings = new Map<number, DisplayString>();
+        let displayId = -1;
+        envProc.stderr.on("data", (data: Buffer) => {
+            for (const line of data.toString().split('\n').filter(s => s.startsWith("@nix "))) {
+                const rawAction: any = JSON.parse(line.substring(5));
+                switch (rawAction.action) {
+                    // Show popups for errors
+                    case "msg": {
+                        const action = rawAction as LogAction;
+                        if (action.level == 0) {
+                            vscode.window.showErrorMessage(action.msg);
+                        }
+                        break;
+                    }
+
+                    // Track relevant progress activities
+                    case "start": {
+                        const action = rawAction as StartAction;
+
+                        // Create message string for relevant activities
+                        let msg: string | undefined;
+                        if (action.type == ActivityType.FileTransfer) {
+                            msg = "Downloading " + leftTruncate(action.fields[0], 24);
+                        } else if (action.type == ActivityType.CopyPath) {
+                            msg = "Fetching " + leftTruncate(action.fields[0], 24);
+                        } else if (action.type == ActivityType.Build) {
+                            msg = "Building " + leftTruncate(action.fields[0], 24);
+                        }
+
+                        // Update activity strings and display ID
+                        if (msg != undefined) {
+                            activityStrings.set(action.id, { s: msg, level: action.level });
+
+                            const displayed = activityStrings.get(displayId);
+                            if (displayed == undefined || action.level <= displayed.level) {
+                                displayId = action.id;
+                            }
+                        }
+
+                        // Start tracking `Builds` and `CopyPaths` activities
+                        if (action.type == ActivityType.Builds || action.type == ActivityType.CopyPaths) {
+                            progress.activities.set(action.id, {
+                                done: 0,
+                                expected: 0,
+                            });
+                        }
+
+                        break;
+                    }
+                    case "stop": {
+                        const action = rawAction as StopAction;
+
+                        // Remove activity from activity strings, potentially
+                        // updating display string to another activity
+                        activityStrings.delete(action.id);
+                        if (displayId == action.id) {
+                            let nextBestLevel = 999;
+                            let nextBestId = -1;
+
+                            for (const [id, { level }] of activityStrings) {
+                                if (level < nextBestLevel) {
+                                    nextBestLevel = level;
+                                    nextBestId = id;
+                                }
+                            }
+
+                            displayId = nextBestId;
+                        }
+
+                        // Flush progress from `Builds` and `CopyPaths`
+                        const prog = progress.activities.get(action.id);
+                        if (prog != undefined) {
+                            progress.done += prog.done;
+                            progress.expected += prog.done;
+                        }
+                        progress.activities.delete(action.id);
+
+                        break;
+                    }
+                    case "result": {
+                        const action = rawAction as ResultAction;
+
+                        // Potentially update display ID
+                        // TODO Filter by result type?
+                        const str = activityStrings.get(action.id);
+                        const displayStr = activityStrings.get(displayId);
+                        if (str != undefined && (displayStr == undefined || str.level <= displayStr.level)) {
+                            displayId = action.id;
+                        }
+
+                        // Update progress on `Builds` and `CopyPaths`
+                        const prog = progress.activities.get(action.id);
+                        if (prog != undefined) {
+                            if (action.type == ResultType.SetExpected) {
+                                prog.expected = action.fields[1];
+                            } else if (action.type == ResultType.Progress) {
+                                prog.done = action.fields[0];
+                                prog.expected = action.fields[1];
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            // Tally up total done and expected activities
+            let totalDone = progress.done;
+            let totalExpected = progress.expected;
+            for (const prog of progress.activities.values()) {
+                totalDone += prog.done;
+                totalExpected += prog.expected;
+            }
+
+            // Update status bar
+            // TODO Show tooltip with full store path
+            const displayString = activityStrings.get(displayId);
+            if (displayString != undefined) {
+                statusBarItem.text = `$(loading~spin) (${totalDone}/${totalExpected}) ${displayString.s}`;
+            } else {
+                statusBarItem.text = `$(loading~spin) (${totalDone}/${totalExpected}) Building Nix environment...`;
+            }
+        });
 
         envProc.on("close", async _code => {
             // Update status bar
